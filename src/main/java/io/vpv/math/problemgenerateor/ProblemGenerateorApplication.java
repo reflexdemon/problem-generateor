a3653998db740d05e5d337d04f57a96b979652a1
reflexdemon <venkateswara.vp@gmail.com>
2018-09-13 17:30:50 -0400

    first commit

---
+++ src/main/java/io/vpv/math/problemgenerateor/ProblemGenerateorApplication.java
@@ -0,0 +1,12 @@
+package io.vpv.math.problemgenerateor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemGenerateorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemGenerateorApplication.class, args);
+	}
+}
