8fc39372b8f51a9231fb4a205c6d6142661f04b1
reflexdemon <venkateswara.vp@gmail.com>
2018-09-17 22:29:41 -0400

    Added Test Coverage Report and Travis integration

--- src/test/java/io/vpv/math/problemgenerateor/service/subtraction/SubtractionProblemServiceTest.java
+++ src/test/java/io/vpv/math/problemgenerateor/service/subtraction/SubtractionProblemServiceTest.java
@@ -1,6 +1,8 @@
 package io.vpv.math.problemgenerateor.service.subtraction;
 
+import io.vpv.math.problemgenerateor.model.ProblemStatement;
 import io.vpv.math.problemgenerateor.service.addition.BaseProblemServiceTest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,9 +12,18 @@ public class SubtractionProblemServiceTest extends BaseProblemServiceTest {
     private SubtractionProblemService subtractionProblemService;
 
     @Test
-    public void generateProblemStatement() {
+    public void shouldGenerateValidProblems() {
         int lowerBound = 10;
         int upperBound = 30;
         assertOperations(subtractionProblemService, lowerBound, upperBound);
     }
+
+    @Test
+    public void shouldHaveFirstNumberAlwaysGreaterThanLast() {
+        int lowerBound = 10;
+        int upperBound = 300;
+        ProblemStatement problemStatement = subtractionProblemService.generateProblemStatement(lowerBound, upperBound);
+        Assert.assertTrue("Should have the first number greater than second", problemStatement.getFirstNumber() > problemStatement.getSecondNumber());
+    }
+
 }
