7dc9687cf518ea44f92124fdfe312c3a35136ca4
reflexdemon <venkateswara.vp@gmail.com>
2019-02-11 11:47:50 -0500

    Fixed the autowiring of the interceptors and bean initilization that was not getting the callback value autowired.

--- src/main/java/io/vpv/math/problemgenerateor/config/SecurityConfig.java
+++ src/main/java/io/vpv/math/problemgenerateor/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.vpv.math.problemgenerateor.config;
 
 import io.vpv.math.problemgenerateor.interceptor.SecurityProtection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private SecurityProtection securityProtection;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SecurityProtection());
+        registry.addInterceptor(securityProtection);
     }
+
 }
--- src/main/java/io/vpv/math/problemgenerateor/interceptor/SecurityProtection.java
+++ src/main/java/io/vpv/math/problemgenerateor/interceptor/SecurityProtection.java
@@ -1,18 +1,33 @@
 package io.vpv.math.problemgenerateor.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class SecurityProtection extends HandlerInterceptorAdapter {
 
     public static final String SESSION_USER = "SESSION_USER";
     public static final String ASK_SESSION = "https://truelogin.vpv.io/whoisthis?callback=";
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Value("${app.callback}")
     private String callback;
+
+    public SecurityProtection() {
+    }
+
+    @PostConstruct
+    public void init() {
+        logger.info("callback value is set to {}", callback);
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
